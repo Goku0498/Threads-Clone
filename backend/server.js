9b4ca635239dfcedd7ceb6a847b99fb2aa602f6a
Gokul Shanmugasundaram <162133861+Goku0498@users.noreply.github.com>
2024-12-27 17:35:07 +0530

    Small Changes

--- backend/server.js
+++ backend/server.js
@@ -25,16 +25,14 @@ cloudinary.config({
 });
 
 // Middlewares
-app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
-app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
-
-// http://localhost:5000 => backend,frontend
 
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));

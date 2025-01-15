eb630b5ba0ce5e18a238989af72096ea940cc6d0
Gokul Shanmugasundaram <162133861+Goku0498@users.noreply.github.com>
2025-01-15 15:23:46 +0530

    removed default post

--- backend/controllers/postController.js
+++ backend/controllers/postController.js
@@ -54,30 +54,6 @@ const getPost = async (req, res) => {
 	}
 };
 
-const getFeedPosts = async (req, res) => {
-  try {
-    const userId = req.user._id;
-    const user = await User.findById(userId);
-    if (!user) {
-      return res.status(404).json({ error: "User not found" });
-    }
-
-    const following = user.following;
-    const defaultUser = await User.findOne({ username: "defaultUser" });
-
-    const feedPosts = await Post.find({
-      $or: [
-        { postedBy: { $in: following } },
-        { postedBy: defaultUser._id },
-      ],
-    }).sort({ createdAt: -1 });
-
-    res.status(200).json(feedPosts);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
-};
-
 const deletePost = async (req, res) => {
 	try {
 		const post = await Post.findById(req.params.id);

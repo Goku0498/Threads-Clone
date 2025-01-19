b7547ee4f79e838b1771d7f5497c3afdfab72dba
Gokul Shanmugasundaram <162133861+Goku0498@users.noreply.github.com>
2025-01-19 23:39:04 +0530

    made changes to homepage

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -10,27 +10,28 @@ const HomePage = () => {
 	const [posts, setPosts] = useRecoilState(postsAtom);
 	const [loading, setLoading] = useState(true);
 	const showToast = useShowToast();
+
 	useEffect(() => {
-		const getFeedPosts = async () => {
-			setLoading(true);
-			setPosts([]);
-			try {
-				const res = await fetch("/api/posts/feed");
-				const data = await res.json();
-				if (data.error) {
-					showToast("Error", data.error, "error");
-					return;
-				}
-				console.log(data);
-				setPosts(data);
-			} catch (error) {
-				showToast("Error", error.message, "error");
-			} finally {
-				setLoading(false);
-			}
-		};
-		getFeedPosts();
-	}, [showToast, setPosts]);
+    const getFeedPosts = async () => {
+        setLoading(true);
+        setPosts([]);
+        try {
+            const res = await fetch("/api/posts/feed");
+            const data = await res.json();
+            if (data.error) {
+                showToast("Error", data.error, "error");
+                return;
+            }
+            console.log("Posts fetched from API:", data);
+            setPosts(data);
+        } catch (error) {
+            showToast("Error", error.message, "error");
+        } finally {
+            setLoading(false);
+        }
+    };
+    getFeedPosts();
+}, [showToast, setPosts]);
 
 	return (
 		<Flex gap='10' alignItems={"flex-start"}>

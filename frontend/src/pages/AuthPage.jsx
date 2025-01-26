d1877bdd7cb521b2a7947e69c739475907862910
Gokul Shanmugasundaram <162133861+Goku0498@users.noreply.github.com>
2025-01-26 07:15:55 +0530

    Modified auth page

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,63 +1,12 @@
-/* eslint-disable no-unused-vars */
-import { useRecoilState } from "recoil";
-import userAtom from "../atoms/userAtom";
+import { useRecoilValue } from "recoil";
 import LoginCard from "../components/LoginCard";
 import SignupCard from "../components/SignupCard";
 import authScreenAtom from "../atoms/authAtom";
 
 const AuthPage = () => {
-    const [authScreenState] = useRecoilState(authScreenAtom);
-    const [user, setUser] = useRecoilState(userAtom);
+	const authScreenState = useRecoilValue(authScreenAtom);
 
-    const handleLogin = async (credentials) => {
-        try {
-            const res = await fetch("/api/users/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(credentials),
-            });
-            const data = await res.json();
-            if (data.error) {
-                // Handle error
-                return;
-            }
-            setUser(data);
-        } catch (error) {
-            // Handle error
-        }
-    };
-
-    const handleSignup = async (userInfo) => {
-        try {
-            const res = await fetch("/api/users/signup", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(userInfo),
-            });
-            const data = await res.json();
-            if (data.error) {
-                // Handle error
-                return;
-            }
-            setUser(data);
-        } catch (error) {
-            // Handle error
-        }
-    };
-
-    return (
-        <>
-            {authScreenState === "login" ? (
-                <LoginCard onLogin={handleLogin} />
-            ) : (
-                <SignupCard onSignup={handleSignup} />
-            )}
-        </>
-    );
+	return <>{authScreenState === "login" ? <LoginCard /> : <SignupCard />}</>;
 };
 
 export default AuthPage;

90b43c00ba9c5d96d6c081067e6dd587eb8d01bc
Gokul Shanmugasundaram <162133861+Goku0498@users.noreply.github.com>
2025-01-26 12:04:37 +0530

    modified some data

--- backend/server.js
+++ backend/server.js
@@ -1,33 +1,32 @@
-import express from "express";
 import path from "path";
-import cookieParser from "cookie-parser";
-import { v2 as cloudinary } from "cloudinary";
-import postRoutes from "./routes/postRoutes.js";
-import userRoutes from "./routes/userRoutes.js";
-import messageRoutes from "./routes/messageRoutes.js";
+import express from "express";
+import dotenv from "dotenv";
 import connectDB from "./db/connectDB.js";
+import cookieParser from "cookie-parser";
+import userRoutes from "./routes/userRoutes.js";
+import postRoutes from "./routes/postRoutes.js";
+import messageRoutes from "./routes/messageRoutes.js";
+import { v2 as cloudinary } from "cloudinary";
+import { app, server } from "./socket/socket.js";
 import job from "./cron/cron.js";
 
-// Connect to the database
-connectDB();
+dotenv.config();
 
-// Start any scheduled jobs
+connectDB();
 job.start();
 
-const app = express();
 const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
-// Cloudinary configuration
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRET,
+	cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+	api_key: process.env.CLOUDINARY_API_KEY,
+	api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 // Middlewares
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: "50mb" })); // To parse JSON data in the req.body
+app.use(express.urlencoded({ extended: true })); // To parse form data in the req.body
 app.use(cookieParser());
 
 // Routes
@@ -35,15 +34,15 @@ app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/messages", messageRoutes);
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+// http://localhost:5000 => backend,frontend
 
-    // React app
-    app.get("*", (_req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+	// react app
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+server.listen(PORT, () => console.log(`Server started at http://localhost:${PORT}`));

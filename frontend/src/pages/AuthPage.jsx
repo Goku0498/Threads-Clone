0090ee932bc45a4e2f92d98c9aabef549f2ce5f3
Gokul Shanmugasundaram <162133861+Goku0498@users.noreply.github.com>
2025-01-26 12:24:04 +0530

    Modified authpage

--- frontend/src/pages/AuthPage.jsx
+++ frontend/src/pages/AuthPage.jsx
@@ -1,46 +1,12 @@
-/* eslint-disable no-unused-vars */
-import { useRecoilState } from "recoil";
+import { useRecoilValue } from "recoil";
 import LoginCard from "../components/LoginCard";
 import SignupCard from "../components/SignupCard";
 import authScreenAtom from "../atoms/authAtom";
-import userAtom from "../atoms/userAtom";
-import { useNavigate } from "react-router-dom";
 
 const AuthPage = () => {
-    const [authScreenState] = useRecoilState(authScreenAtom);
-    const [user, setUser] = useRecoilState(userAtom);
-    const navigate = useNavigate();
+	const authScreenState = useRecoilValue(authScreenAtom);
 
-    const handleSignup = async (userInfo) => {
-        try {
-            const res = await fetch("/api/users/signup", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify(userInfo),
-            });
-            const data = await res.json();
-            if (data.error) {
-                // Handle error
-                return;
-            }
-            setUser(data);
-            navigate("/suggested-users");
-        } catch (error) {
-            // Handle error
-        }
-    };
-
-    return (
-        <>
-            {authScreenState === "login" ? (
-                <LoginCard />
-            ) : (
-                <SignupCard onSignup={handleSignup} />
-            )}
-        </>
-    );
+	return <>{authScreenState === "login" ? <LoginCard /> : <SignupCard />}</>;
 };
 
 export default AuthPage;
